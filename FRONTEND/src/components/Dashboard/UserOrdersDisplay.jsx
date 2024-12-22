cfc4ecd50d699e6be225636612cc00df28a4c2c5
Meghana <meghanamomidi@gmail.com>
2024-12-22 13:11:28 +0530

    Modified users images in dashboard page

--- FRONTEND/src/components/Dashboard/UserOrdersDisplay.jsx
+++ FRONTEND/src/components/Dashboard/UserOrdersDisplay.jsx
@@ -1,15 +1,6 @@
 import React from "react";
-const userImages = [
-  "women/82.jpg",
-  "men/81.jpg",
-  "men/80.jpg",
-  "men/79.jpg",
-  "women/78.jpg",
-  "women/77.jpg",
-  "men/76.jpg",
-  "men/75.jpg",
-];
-const UserOrdersDisplay = ({data}) => {
+const person = ["women", "men"];
+const UserOrdersDisplay = ({ data }) => {
   return (
     <>
       <div className="flex items-center justify-between px-6 py-5 font-semibold border-b border-gray-100 dark:text-gray-300">
@@ -24,13 +15,17 @@ const UserOrdersDisplay = ({data}) => {
                 <div className="h-10 w-10 mr-3 bg-gray-100 rounded-full overflow-hidden">
                   <img
                     src={`https://randomuser.me/api/portraits/${
-                      userImages[Math.floor(Math.random() * userImages.length)]
-                    }`}
+                      person[Math.floor(Math.random() * person.length)]
+                    }/${Math.floor(Math.random() * 100 + 10)}.jpg`}
                     alt={`${userName} profile picture`}
                   />
                 </div>
-                <span className="text-gray-600 dark:text-white">{userName}</span>
-                <span className="ml-auto font-semibold dark:text-white">{orders}</span>
+                <span className="text-gray-600 dark:text-white">
+                  {userName}
+                </span>
+                <span className="ml-auto font-semibold dark:text-white">
+                  {orders}
+                </span>
               </li>
             );
           })}
