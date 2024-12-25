f1fc6b8a3fcf801e666d5598127df720b7a47dc1
Meghana <meghanamomidi@gmail.com>
2024-12-26 00:21:13 +0530

    Added vercel backend url in getBaseURL

--- FRONTEND/src/utils/baseURL.js
+++ FRONTEND/src/utils/baseURL.js
@@ -1,6 +1,11 @@
 const getBaseURL = () => {
+  // Check if the app is running in production (Vercel)
+  if (process.env.NODE_ENV === 'production') {
+    return "https://book-store-backend-olive.vercel.app/"; // Vercel backend URL
+  }
+  
+  // Default to localhost in development
   return "http://localhost:3000";
 };
 
 export default getBaseURL;
- 
