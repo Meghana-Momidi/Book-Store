affdd0359179f0ca0f54fa1d8a26d1ee6480c24f
Meghana <meghanamomidi@gmail.com>
2024-12-25 22:38:22 +0530

    Added serverless functionality in index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
+const serverless = require("serverless-http");
 
 const bookRoutes = require("./src/books/book-routes");
 const orderRoutes = require("./src/orders/order-routes");
@@ -11,21 +12,20 @@ const adminStats = require("./src/stats/admin-stats");
 const HttpError = require("./src/models/http-error");
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 // Middleware setup
-app.use(express.json()); // Parse JSON requests
+app.use(express.json());
+app.use(morgan("dev")); // Log HTTP requests
+
 app.use(
   cors({
     origin: [
-      "http://localhost:5173", // For local development
-      "https://book-store-ivory-mu.vercel.app" // For production on Vercel
+      "http://localhost:5173", // Local dev URL
+      "https://book-store-8sijy0voy-meghana-momidis-projects.vercel.app", // Production URL
     ],
     credentials: true,
   })
 );
-
-app.use(morgan("dev")); // Log HTTP requests in a concise, colored format (for development)
 
 // Route setup
 app.use("/api/books", bookRoutes);
@@ -38,15 +38,13 @@ app.get("/", (req, res) => {
   res.send("Book Store server is running!");
 });
 
-// Error handling middleware for undefined routes and errors
+// Error handling
 app.use((error, req, res, next) => {
-  // If it's a custom HttpError, use its send method to send the error response
   if (error instanceof HttpError) {
     return error.send(res);
   }
 
-  // Default error handler if it's not a custom HttpError
-  console.error(error); // Log the error to the console
+  console.error(error);
   res.status(500).json({
     message: "An unexpected error occurred. Please try again later.",
     error: error.message,
@@ -67,20 +65,12 @@ async function connectToDatabase() {
   }
 }
 
-// Server start function
+// Run the serverless function handler instead of app.listen
 async function startServer() {
   await connectToDatabase();
-  app.listen(port, () => {
-    console.log(`Server is listening on port ${port}...`);
-  });
 }
 
-// Start the server
-startServer();
+// Export the app for serverless deployment
+module.exports.handler = serverless(app); // Export for serverless
 
-// Graceful shutdown on process termination
-process.on("SIGINT", async () => {
-  console.log("Shutting down server...");
-  await mongoose.disconnect(); // Disconnect from MongoDB
-  process.exit(0); // Exit the process
-});
+startServer();
