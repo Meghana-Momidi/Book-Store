afd3cdccc36d1150d72f724a998c85452d1b3371
Meghana <meghanamomidi@gmail.com>
2024-12-26 00:23:27 +0530

    Corrected backend url in baseURL

--- FRONTEND/src/utils/baseURL.js
+++ FRONTEND/src/utils/baseURL.js
@@ -1,7 +1,7 @@
 const getBaseURL = () => {
   // Check if the app is running in production (Vercel)
   if (process.env.NODE_ENV === 'production') {
-    return "https://book-store-backend-olive.vercel.app/"; // Vercel backend URL
+    return "https://book-store-backend-olive.vercel.app"; // Vercel backend URL
   }
   
   // Default to localhost in development
