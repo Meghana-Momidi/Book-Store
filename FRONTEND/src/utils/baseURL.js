da4880a3a18035b5772001dfe2eabc21f23856fa
Meghana <meghanamomidi@gmail.com>
2024-12-25 23:15:40 +0530

    Modified baseURL.js

--- FRONTEND/src/utils/baseURL.js
+++ FRONTEND/src/utils/baseURL.js
@@ -1,6 +1,9 @@
 const getBaseURL = () => {
-  return "http://localhost:3000";
+  if (process.env.NODE_ENV === "development") {
+    return "http://localhost:3000"; // Local development URL
+  } else {
+    return "https://book-store-backend-ouepyd5kx-meghana-momidis-projects.vercel.app"; // Deployed backend URL on Vercel
+  }
 };
 
 export default getBaseURL;
- 
